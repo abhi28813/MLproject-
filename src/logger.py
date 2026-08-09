572076338d5a2aeaca313151eeb8e8109e047bcb
Abhijith <abhijithkraj28@gmail.com>
2026-08-09 18:30:18 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+import datetime as dt
+
+LOG_FILE_NAME = f"log_{dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+log_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_path, exist_ok=True)
+
+LOG_FILE_PATH =os.path.join(log_path, LOG_FILE_NAME)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH, 
+    format="[%(asctime)s] %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+if __name__ == "__main__":
+    logging.info("Logging has started.")
+
+
